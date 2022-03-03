60502c1fe5be1b1da04c31cf77ad87bd5b511c30
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-03 15:48:08 -0500

    Javascript file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+  window.alert("The Fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?")
+
+// fight();
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var myName = "your name";
+console.log(myName);
